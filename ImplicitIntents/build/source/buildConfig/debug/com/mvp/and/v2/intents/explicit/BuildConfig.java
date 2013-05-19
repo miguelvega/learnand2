d7d4758388d59bdb37fc4d688f95160a8499dc77
Miguel Vega <mikevegap@gmail.com>
2013-05-19 16:57:54 -0400

    Intent Filter created

--- ImplicitIntents/build/source/buildConfig/debug/com/mvp/and/v2/intents/explicit/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.mvp.and.v2.intents.explicit;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
--- NewIntentFilter/src/main/java/com/mvp/and/v2/intents/builder/MainActivity.java
+++ NewIntentFilter/src/main/java/com/mvp/and/v2/intents/builder/MainActivity.java
@@ -1,15 +1,54 @@
 package com.mvp.and.v2.intents.builder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.StrictMode;
 import android.view.Menu;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        //para obtener la accion del uso del Intent
+        String action = intent.getAction();
+        if(!action.equals(Intent.ACTION_VIEW)){
+            throw new RuntimeException("This should never happen");
+        }
+
+        //para obtener el uso de datos
+        Uri data = intent.getData();
+        URL url;
+
+        try {
+            url = new URL(data.getScheme(), data.getHost(), data.getPath());
+            BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                textView.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
